e97847fa97fd55ec1481a9f4208626d023cb78f6
phuonglam94 <51202846@hcmut.edu.vn>
2016-04-03 22:22:28 +0700

    fix \n

--- src/main/java/com/phuonglam/database/Database.java
+++ src/main/java/com/phuonglam/database/Database.java
@@ -117,7 +117,7 @@ public class Database {
         try {
             System.out.println(userName + " " + password + " " + longi + " " + latti);
             if (userName != null && password != null) {
-                if (password.charAt(password.length()-1)==61){
+                if (password.charAt(password.length()-1)==10){
                     password=password.substring(0, password.length()-1);
                 }
                 String SQL = "SELECT id FROM userdb WHERE username = '" + userName + "' AND password = '" + password + "';";
